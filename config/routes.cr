40bb83622e5214c74ae3d94aefbf28bbbd33eafe
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-22 23:09:25 +0900

    Add admin routes

--- config/routes.cr
+++ config/routes.cr
@@ -1,6 +1,6 @@
 Amber::Server.configure do |app|
   pipeline :web do
-  
+
     # Plug is the method to use connect a pipe (middleware)
     # A plug accepts an instance of HTTP::Handler
     plug Amber::Pipe::Error.new
@@ -10,7 +10,7 @@ Amber::Server.configure do |app|
     plug Amber::Pipe::CSRF.new
     # Reload clients browsers (development only)
     plug Amber::Pipe::Reload.new if Amber.env.development?
-  
+
     plug Authenticate.new
   end
 
@@ -27,17 +27,23 @@ Amber::Server.configure do |app|
   end
 
   routes :web do
-      resources "/post_comments", PostCommentController
-      resources "/posts", PostController
-      resources "/categories", CategoryController
-      get "/profile", UserController, :show
-  get "/profile/edit", UserController, :edit
-  patch "/profile", UserController, :update
-  get "/signin", SessionController, :new
-  post "/session", SessionController, :create
-  get "/signout", SessionController, :delete
-  get "/signup", RegistrationController, :new
-  post "/registration", RegistrationController, :create
+    resources "/post_comments", PostCommentController
+    resources "/posts", PostController, only: [:index, :show]
+    resources "/categories", CategoryController
+    get "/profile", UserController, :show
+    get "/profile/edit", UserController, :edit
+    patch "/profile", UserController, :update
+    get "/signin", SessionController, :new
+    post "/session", SessionController, :create
+    get "/signout", SessionController, :delete
+    get "/signup", RegistrationController, :new
+    post "/registration", RegistrationController, :create
     get "/", HomeController, :index
+  end
+
+  routes :web, "/admin" do
+    resources "/post_comments", PostCommentController
+    resources "/posts", PostController
+    resources "/categories", CategoryController
   end
 end
